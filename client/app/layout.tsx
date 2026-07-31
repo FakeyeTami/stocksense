4531027cfd0df4f04a347dbd8238226617bc53ce
Tamicodes <fakeyetami+github@gmail.com>
2026-07-31 14:07:31 +0100

    refactor: updated styling and app layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,19 +3,19 @@ import { IBM_Plex_Mono, IBM_Plex_Sans, IBM_Plex_Serif } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
 
-const fontSans = IBM_Plex_Sans({
+const sans = IBM_Plex_Sans({
     subsets: ["latin"],
     variable: "--font-sans",
     weight: ["400", "500", "600", "700"],
 });
 
-const fontSerif = IBM_Plex_Serif({
+const serif = IBM_Plex_Serif({
     subsets: ["latin"],
     variable: "--font-serif",
     weight: ["400", "500", "700"],
 });
 
-const fontMono = IBM_Plex_Mono({
+const mono = IBM_Plex_Mono({
     subsets: ["latin"],
     variable: "--font-mono",
     weight: ["400", "500", "700"],
@@ -35,7 +35,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
+                className={`${sans.variable} ${serif.variable} ${mono.variable} antialiased`}
             >
                 {children}
                 <Toaster />
